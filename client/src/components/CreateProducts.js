299c96c8037b0616e91b014f3082436106d492ca
veeraa1270@gmail.com <veeraa1270@gmail.com>
2022-09-07 15:32:34 -0500

    ruteo react, fase inicial crear producto

---
+++ client/src/components/CreateProducts.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+import React, {useState} from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const CreateProducts = () => {
+  return (
+    <div>CreateProducts</div>
+  )
+}
+
+export default CreateProducts
