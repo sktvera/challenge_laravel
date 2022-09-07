5814a70b9fc54ad42cccd1d438c9746c4af7e069
veeraa1270@gmail.com <veeraa1270@gmail.com>
2022-09-07 11:42:25 -0500

    importando comnponentes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+
+import ShowProducts from './components/ShowProducts';
+
 function App() {
   return (
     <div className="App">
