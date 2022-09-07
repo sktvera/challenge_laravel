e61ab967a237d5bcaae33391d571e4def8513688
veeraa1270@gmail.com <veeraa1270@gmail.com>
2022-09-07 11:42:55 -0500

    importando componentes,implementando delete

--- client/src/components/ShowProducts.js
+++ client/src/components/ShowProducts.js
@@ -18,11 +18,41 @@ setProducts(response)
 
 }
 
-const deleteProducts = () => {
-
+const deleteProducts = async(id) => {
+await axios.delete(`${endpoint}/products/${id}`)
+getAllProducts()
 }
     return(
-        <div>Table</div>
+        <div>
+            <div className="d-grid gap-2">      
+                <link to="create" className="btn btn-success btn-lg mt-2 text-white">create</link>
+            <table>
+                <thead className="table table-striped">
+                    <tr>
+                        <th>Description</th>
+                        <th>Price</th>
+                        <th>Stock</th>
+                        <th>Action</th>
+
+                    </tr>
+                </thead>
+
+                <tbody className="">
+                    {products.map((product)=>(
+                        <tr key={product.id}>
+                            <td>{product.description} </td>
+                            <td>{product.price} </td>
+                            <td>{product.stock} </td>
+                            <td>
+                                <Link to={`/edit/${product.id}`} className='btn btn-warning' >Edit</Link>
+                                <button onClick={()=>deleteProduct(product.id)} className='btn btn-danger' >Delete</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            </div>
+        </div>
     )
 }
 
